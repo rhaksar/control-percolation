d851eceb74f73bb4fb7a674e4cf76e335f2287ee
Ravi Haksar <rhakczar@gmail.com>
2019-02-09 16:24:11 -0800

    Implemented effect of uniform boundary treatment policy for the branching process analysis.

--- Policies.py
+++ Policies.py
@@ -4,10 +4,11 @@ import numpy as np
 
 class Policy(object):
 
-    def __init__(self, capacity):
+    def __init__(self, capacity, delta_p):
         self.capacity = capacity
+        self.delta_p = delta_p
 
-    def probability(self, *args):
+    def effect(self, *args):
         raise NotImplementedError
 
     def control(self, *args):
@@ -19,12 +20,16 @@ class UBTfires(Policy):
     Choose boundary nodes to apply action with uniform probability.
     """
 
-    def __init__(self, capacity):
-        Policy.__init__(self, capacity)
+    def __init__(self, capacity, delta_p):
+        Policy.__init__(self, capacity, delta_p)
 
-    def probability(self, boundary_size):
-        return np.amin([float(self.capacity)/boundary_size, 1])
+    def effect(self, branchmodel, gw):
+        boundary_size = branchmodel.statistics[branchmodel.generations]['mean']
+        if boundary_size == 0:
+            return self.delta_p
 
-    def control(self, boundary):
+        return np.amin([float(self.capacity)/boundary_size, 1])*self.delta_p
+
+    def control(self, branchmodel):
         control = defaultdict(lambda: (0, 0))
         pass
